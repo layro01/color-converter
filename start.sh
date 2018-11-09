11953e507891d59b61c9080c07d316c638c47586
Rob Layzell <robert.layzell@ca.com>
2018-11-09 11:48:51 -0800

    Re-added Hailstone Agent run settings.

--- start.sh
+++ start.sh
@@ -10,13 +10,13 @@
 # These should be fine set at their default values.
 # If we want a user to be able to set them, we could update the Jenkins Plugin so 
 # they could be configured as necessary.
-# export IASTAGENT_LOGGING_STDERR_LEVEL=info
-# export IASTAGENT_LOGGING_FILE_ENABLED=true
-# export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
-# export IASTAGENT_LOGGING_FILE_LEVEL=info
-# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
-# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
-# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
+export IASTAGENT_LOGGING_STDERR_LEVEL=info
+export IASTAGENT_LOGGING_FILE_ENABLED=true
+export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
+export IASTAGENT_LOGGING_FILE_LEVEL=info
+export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
+export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
+export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
 
 # The BUILD_TAG comes from the Jenkins environment, however you can enable the following line
 # if you want to run the script manually and mock a Jenkins build execution.
