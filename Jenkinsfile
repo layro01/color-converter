e1fe913b68678a6d13492f0459c80f525aa48379
Rob Layzell <robert.layzell@ca.com>
2019-01-16 17:16:54 -0800

    Change Agent Server back to localhost.

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,9 +12,9 @@ pipeline {
     }
     stage('Test') {
       steps {
-        wrap([$class: 'HailstoneBuildWrapper', location: 'agent-server', port: '10010']) {
+        wrap([$class: 'HailstoneBuildWrapper', location: 'localhost', port: '10010']) {
           sh 'forever start -e err.log --killSignal SIGTERM --minUptime 1000 --spinSleepTime 1000 -c /bin/sh ./start.sh'
-          sleep(time:10,unit:"SECONDS")
+          sh 'while [ $(curl -s -w "%{http_code}" "http://localhost:3000/echo?text=hello" -o /dev/null) != "200" ]; do sleep 5 ; done'
           // Comment in this next line to view the Agent log.
           // sh 'cat err.log'
           sh 'npm test'
--- start.sh
+++ start.sh
@@ -10,15 +10,15 @@
 # These should be fine set at their default values.
 # If we want a user to be able to set them, we could update the Jenkins Plugin so 
 # they could be configured as necessary.
-export IASTAGENT_LOGGING_STDERR_LEVEL=info
+export IASTAGENT_LOGGING_STDERR_LEVEL=debug
 export IASTAGENT_LOGGING_FILE_ENABLED=true
 export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
-export IASTAGENT_LOGGING_FILE_LEVEL=info
+export IASTAGENT_LOGGING_FILE_LEVEL=debug
 export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
 export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
 export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
 export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=agent-server
+export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
 export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
 
 # The BUILD_TAG comes from the Jenkins environment, however you can enable the following line
