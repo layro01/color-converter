b5d551a5d8f1d270bab71c73b8fac6e7527c9ba8
Rob Layzell <robert.layzell@ca.com>
2019-01-24 09:17:57 -0800

    Debug info...

--- start.sh
+++ start.sh
@@ -10,19 +10,24 @@
 # These should be fine set at their default values.
 # If we want a user to be able to set them, we could update the Jenkins Plugin so 
 # they could be configured as necessary.
-export IASTAGENT_LOGGING_STDERR_LEVEL=debug
-export IASTAGENT_LOGGING_FILE_ENABLED=true
-export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
-export IASTAGENT_LOGGING_FILE_LEVEL=debug
-export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
-export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
-export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
+# export IASTAGENT_LOGGING_STDERR_LEVEL=debug
+# export IASTAGENT_LOGGING_FILE_ENABLED=true
+# export IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
+# export IASTAGENT_LOGGING_FILE_LEVEL=debug
+# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
+# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
+# export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
 
 # The BUILD_TAG comes from the Jenkins environment, however you can enable the following line
 # if you want to run the script manually and mock a Jenkins build execution.
 # export BUILD_TAG=jenkins-agent-server-test-pipeline-9000
 
+echo Starting Node app...
+echo PATH=$PATH
+echo NODE_PATH=$NODE_PATH
+echo PWD=$(pwd)
+ls
 node -r /agent_nodejs_linux64 app/server.js
