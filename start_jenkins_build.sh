fe9c560407f4e9cdf5bd18fa36238b610351585e
Rob Layzell <robert.layzell@ca.com>
2019-01-25 18:23:50 -0800

    More debugging.

--- start_jenkins_build.sh
+++ start_jenkins_build.sh
@@ -4,4 +4,5 @@ STARTCRUMB=$(curl -s --user "admin:admin" http://localhost:8080/crumbIssuer/api/
 
 CRUMB="Jenkins-Crumb:${STARTCRUMB:1}"
 
-curl -s -X POST "http://localhost:8080/job/color-converter/build" -H "$CRUMB" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":"docker", "IAST_SERVER_PORT":"10010", "IAST_AGENT_PATH": "~/home/agents"}]}'
+# curl -s -X POST "http://localhost:8080/job/color-converter/build" -H "$CRUMB" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":"docker", "IAST_SERVER_PORT":"10010", "IAST_AGENT_PATH": "~/home/agents"}]}'
+curl -s -X POST "http://localhost:8080/job/color-converter/build" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":"host.docker.internal", "IAST_SERVER_PORT":"10010", "IAST_AGENT_PATH": "~/home/agents"}]}'
