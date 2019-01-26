8ee7d1523db2123247562810dc023f167a0c1c7c
Rob Layzell <robert.layzell@ca.com>
2019-01-25 19:08:22 -0800

    Draft of Jenkins automation script.

--- start_jenkins_build.sh
+++ start_jenkins_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-STARTCRUMB=$(curl -s --user "admin:admin" http://localhost:8080/crumbIssuer/api/xml?xpath=concat\(//crumRequestField,%22:%22,//crumb\))
+IAST_SERVER_HOST=host.docker.internal
+IAST_SERVER_PORT=10010
+IAST_AGENT_PATH=/home/agents
+JENKINS_URL=http://localhost:8080
+JENKINS_PROJECT=color-converter
+
+STARTCRUMB=$(curl -s --user "admin:admin" ${JENKINS_URL}/crumbIssuer/api/xml?xpath=concat\(//crumRequestField,%22:%22,//crumb\))
 
 CRUMB="Jenkins-Crumb:${STARTCRUMB:1}"
 
-# curl -s -X POST "http://localhost:8080/job/color-converter/build" -H "$CRUMB" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":"docker", "IAST_SERVER_PORT":"10010", "IAST_AGENT_PATH": "~/home/agents"}]}'
-curl -s -X POST "http://localhost:8080/job/color-converter/build" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":"host.docker.internal", "IAST_SERVER_PORT":"10010", "IAST_AGENT_PATH": "~/home/agents"}]}'
+# curl -s -X POST "${JENKINS_URL}/job/${JENKINS_PROJECT}/build" -H "$CRUMB" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":${IAST_SERVER_HOST}, "IAST_SERVER_PORT":"${IAST_SERVER_PORT}", "IAST_AGENT_PATH": "${IAST_AGENT_PATH}"}]}'
+curl -s -X POST "${JENKINS_URL}/job/${JENKINS_PROJECT}/build" --data-urlencode json='{"parameter": [{"IAST_SERVER_HOST":${IAST_SERVER_HOST}, "IAST_SERVER_PORT":"${IAST_SERVER_PORT}", "IAST_AGENT_PATH": "${IAST_AGENT_PATH}"}]}'
