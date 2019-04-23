5a85179250c5d1f7934de4a092f165c2c7916829
Rob Layzell <rlayzell@veracode.com>
2019-04-23 11:43:41 -0700

    Add a batch file for starting forever process so it's easier to debug.

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM These should be fine set at their default values.
+REM If we want a user to be able to set them, we could update the Jenkins Plugin so 
+REM they could be configured as necessary.
+set IASTAGENT_LOGGING_STDERR_LEVEL=info
+REM set IASTAGENT_LOGGING_FILE_ENABLED=true
+REM set IASTAGENT_LOGGING_FILE_PATHNAME=iastdebug.txt
+REM set IASTAGENT_LOGGING_FILE_LEVEL=info
+REM set IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
+REM set IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
+REM set IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
+REM set IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
+REM set IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
+REM set IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
+
+node -r .\agent_nodejs_win64 app/server.js
