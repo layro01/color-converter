22c38fba9c599e427319fb8e69fe70416efd2489
Rob Layzell <rlayzell@veracode.com>
2019-10-11 14:32:39 -0700

    Tidy up and useful strace command.

--- start.sh
+++ start.sh
@@ -10,13 +10,15 @@ export IASTAGENT_LOGGING_STDERR_LEVEL=info
 # export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_ENABLED=true
 # export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_PATHNAME=iastoutput.ndjson
 # export IASTAGENT_ANNOTATIONHANDLER_JSONFILE_LEVEL=info
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
-export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_ENABLED=true
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_LOCATION=localhost
+# export IASTAGENT_REMOTE_ENDPOINT_HTTP_PORT=10010
 
 # You should not need to set a build tag before starting the app with the IAST agent attached.
 # export BUILD_TAG=dummy
 
-export NODE_PATH=~/vscode/hailstone/iast-dev/out/agent/Debug/nodejs
+# curl -sSL https://s3.us-east-2.amazonaws.com/app.veracode-iast.io/iast-ci.sh | sh
 
-node -r ./agent_nodejs_darwin64 app/server.js
+# export LD_LIBRARY_PATH=$PWD
+node -r ./agent_linux64.node app/server.js
+# strace node -r ./agent_linux64 app/server.js 1>startup.strace.log 2>&1
